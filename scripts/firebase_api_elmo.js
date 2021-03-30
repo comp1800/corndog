b0ff08837ee3575bd4105d1ef749116db67070f7
Carly Orr <carly_wong_orr@bcit.ca>
2021-03-30 00:43:25 -0700

    first commit

---
+++ scripts/firebase_api_elmo.js
@@ -0,0 +1,21 @@
+//---------------------------------------------------
+// replace the lines below with your own "firebaseConfig"
+// key value pairs
+//--------------------------------------------------- 
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: 
+  authDomain: 
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId:
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Get a reference to database
+const db = firebase.firestore();
+// Get a reference to the storage service, which is used to create references in your storage bucket
+// const storage = firebase.storage();
